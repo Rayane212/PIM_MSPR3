fe711a3e1ed4739444c9e8acd99f61d5526c5455
Rayane Hadi <rayanehadi41@gmail.com>
2023-07-28 15:27:41 +0200

    ajout du front

---
+++ PIM_MSPR3/Front/JS/app.js
@@ -0,0 +1,11 @@
+function redirectToCreateAccountPage() {
+    window.location.href = "SignUp.html";
+  }
+
+  function redirectToConnectPage() {
+    window.location.href = "Login.html";
+  }
+
+  function redirectToHomePage() {
+    window.location.href = "Produit.html";
+  }
